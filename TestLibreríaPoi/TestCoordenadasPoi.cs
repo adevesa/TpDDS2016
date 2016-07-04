d413f93eb714835e8f8aaa79d0b48e2bb7cbeb2a
ezequieloscarescobar <ezequieloscarescobar2@gmail.com>
2016-07-04 16:37:10 -0300

    Test UBICACION POR COORDENADA DE GOOGLE -> Debe fallar para que se aprecie que NOS DEVUELVE UN VALOR REAL DE COORDENADA GEOGRAFICA

---
+++ TestLibreríaPoi/TestCoordenadasPoi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibreriaClasesPoi;
+using GoogleApi;
+
+namespace TestLibreríaPoi
+{
+    [TestClass]
+    public class TestCoordenadasPoi
+    {
+        Poi abastoShopping = new LocalComercial(8, "abasto shopping", "avenida corrientes 3274", new Coordenada(-30, -58));
+
+        [TestMethod]
+        public void UbicacionPorCoordenadasGoogle()
+        {
+            bool esUbi =abastoShopping.EsUbicable();
+            Assert.IsTrue(esUbi);
+
+
+            double latitudGoogleana = abastoShopping.getCoordenada().latitud;
+            Assert.IsFalse(-30 == latitudGoogleana );
+
+            //Por buscar en Google maps (A mano desde el navegador) obtenemos:
+            double esperado = -34.603329;
+
+
+            Assert.AreEqual(esperado, latitudGoogleana, "no es correcta");
+        }
+    }
+}
